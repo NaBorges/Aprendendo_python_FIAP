69aa30d7a73c09f1891e10497b891b85dc39779a
NaBorges <reinaldo.gege1@outlook.com>
2025-04-07 22:51:15 -0300

    Aprendendo if simples

--- BOOLEANM/Restaurante_quilo.py
+++ BOOLEANM/Restaurante_quilo.py
@@ -6,3 +6,6 @@ peso_prato = float(input("Informe o peso do prato do cliente(em kg)"))
 preco_prato = preco_quilo* peso_prato
 
 print(f"O valor do prato é{preco_prato: .2f}")
+
+if peso_prato > 1:
+    print("Como o peso do prato do cliente ultrapassou 1kg, ele deve pagar apenas o valor fixo de R$80,00")
