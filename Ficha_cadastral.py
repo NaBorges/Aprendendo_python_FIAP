5c9e2cb0d15e5a28b879400686e75cc0d9afc110
NaBorges <reinaldo.gege1@outlook.com>
2025-04-08 18:18:48 -0300

    Aprendendo if simples

--- Ficha_cadastral.py
+++ Ficha_cadastral.py
@@ -12,10 +12,12 @@ altura = int(input("Por favor, informe a sua altura em cm: "))
 ano_nascimento = int(input("Por favor, informe seu ano de nascimento"))
 
 
-idade = 2025- ano_nascimento
+idade = 2025 - ano_nascimento
 tem_peso_minimo = peso > 50
 tem_idade_minima = idade >= 16
 
 texto_saida = f"\tnome: {nome}\n\tPeso: {peso} kg\n\tALTURA: {altura} cm\n\tIDADE:{idade}\n\tTEM PESO MINIMO PARA DOAR: {tem_peso_minimo}\n\t Tem idade minima para doar:  {tem_idade_minima}"
 
 print(texto_saida)
+
+print("termino do projeto")
